6cab5872e741bb2e9450b4a95b472db5445d9787
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-10-05 12:27:25 +0800

    小修小布辣

--- src/utils/SettingListeners.js
+++ src/utils/SettingListeners.js
@@ -1,4 +1,6 @@
-const ecAPI = window.encrypt_chat
+import {pluginLog} from "./frontendLogUtils.js";
+
+const csAPI = window.change_summary
 
 export class SettingListeners {
     constructor(doc) {//传入一个document对象
@@ -6,54 +8,34 @@ export class SettingListeners {
     }
 
     async keyInputListener() {
-        let keyValue = undefined
-        const keyInputEl = this.document.querySelector('#ec-key-input')
-        keyInputEl.value = (await ecAPI.getConfig()).encryptionKey
+        let picValue = undefined
+        let memeValue = undefined
 
-        keyInputEl.addEventListener('change', async event => {
-            keyValue = event.target.value
+        const picInputEl = this.document.querySelector('#cs-input-pic')
+        const memeInputEl = this.document.querySelector('#cs-input-meme')
+        picInputEl.value = (await csAPI.getConfig()).picOutsideText
+        memeInputEl.value = (await csAPI.getConfig()).memeOutsideText
 
-            // 发送设置密钥事件
-            await ecAPI.setConfig({encryptionKey: keyValue})
-            console.log('修改密钥为' + keyValue)
-        })
-    }
-
-    async colorSelectorListener() {
-        let keyValue = undefined
-        const colorSelEl = this.document.querySelector('#ec-color-selector')
-        colorSelEl.value = (await ecAPI.getConfig()).mainColor
-
-        colorSelEl.addEventListener('change', async event => {
-            keyValue = event.target.value
+        //设置监听器
+        picInputEl.addEventListener('change', async event => {
+            picValue = event.target.value
 
             // 发送设置密钥事件
-            await ecAPI.setConfig({mainColor: keyValue})
-            //rePatchCss()
-            //不应该在这里调用rePatchCss，因为窗口不对。在这里是对设置窗口本身修改，没用。
-            //在setConfig里有设置。如果修改了主题色，主进程会对所有聊天窗口发送ipcMsg。
-            //渲染进程收到后进行修改主题色。
-            console.log('[EC]修改主题色为' + keyValue)
+            await csAPI.setConfig({picOutsideText: picValue})
+            pluginLog('修改图片外显为' + picValue)
         })
-    }
 
-    async styleSelectorListener() {
-        let keyValue = undefined
-        const styleSelEl = this.document.querySelector('#ec-style-selector')
-        styleSelEl.value = (await ecAPI.getConfig()).currentStyleName
-
-        styleSelEl.addEventListener('change', async event => {
-            keyValue = event.target.value
+        memeInputEl.addEventListener('change', async event => {
+            memeValue = event.target.value
 
             // 发送设置密钥事件
-            await ecAPI.setConfig({currentStyleName: keyValue})
-            console.log('[EC]语种设置为'+keyValue)
+            await csAPI.setConfig({memeOutsideText: memeValue})
+            pluginLog('修改表情包外显为' + memeValue)
         })
+
     }
 
     onLoad() {
         this.keyInputListener()
-        this.colorSelectorListener()
-        this.styleSelectorListener()
     }
 }
---
+++ src/utils/frontendLogUtils.js
@@ -0,0 +1,12 @@
+const pluginName = hexToAnsi('#00a8ff') + "[Change Summary] " + '\x1b[0m'
+
+function hexToAnsi(hex) {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+    return `\x1b[38;2;${r};${g};${b}m`;
+}
+
+export function pluginLog(message) {
+    return console.log(pluginName + message)
+}
