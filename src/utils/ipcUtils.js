05281af0d0a40ef1f686e604d8490afd4189058e
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-06-13 17:23:09 +0800

    feat(ipc-modify): support `32xxx`+
    close #12

--- src/utils/ipcUtils.js
+++ src/utils/ipcUtils.js
@@ -1,5 +1,5 @@
-const {Config} = require("../Config.js")
-const {pluginLog} = require("./backendLogUtils.js");
+const { Config } = require("../Config.js")
+const { pluginLog } = require("./backendLogUtils.js");
 
 const config = Config.config
 
@@ -16,11 +16,11 @@ function ipcModifyer(ipcProxy, window) {
             let modifiedArgs = args;
             try { // thisArg是WebContent对象
                 // 设置ipc通道名
-                const ipcName = args?.[3]?.[1]?.[0];
+                const ipcName = args?.[3]?.[1]?.[0] || args?.[3]?.[1]?.cmdName;
                 const eventName = args?.[3]?.[0]?.eventName;
 
                 if (ipcName === 'nodeIKernelMsgService/sendMsg')
-                    modifiedArgs = await ipcMsgModify(args, window);
+                    modifiedArgs = await ipcMsgModify(args, ipcName, window);
 
                 return await target.apply(thisArg, modifiedArgs);
             } catch (err) {
@@ -32,61 +32,65 @@ function ipcModifyer(ipcProxy, window) {
 }
 
 
-async function ipcMsgModify(args) {
+async function ipcMsgModify(args, cmdName) {
     // 获取随机文本辅助函数
     const getRandomText = async () => {
         const response = await fetch(config.randomTextApi);
         const json = await response.json();
-        if(!response.ok || !json.hasOwnProperty(config.randomTextApiKey))
+        if (!response.ok || !json.hasOwnProperty(config.randomTextApiKey))
             return false;
         return json[config.randomTextApiKey];
     }
 
-    if (!args?.[3]?.[1]?.[0] || args[3][1][0] !== 'nodeIKernelMsgService/sendMsg') return args;
+    if (!cmdName || cmdName !== 'nodeIKernelMsgService/sendMsg') return args;
 
+    const payload = Array.isArray(args[3][1][1]) ? args[3][1][1] : args[3][1].payload;
+    // console.log('modmsg', payload);
     //修改原始消息
-    for (let item of args[3][1][1].msgElements) {
-        //说明消息内容是文字类
-        if (item.elementType === 1) {
-            //修改解密消息
-            //item.textElement.summary='嘻嘻哈哈'   //这个修改没有用
-        }
-
-        //说明消息内容是图片类，则修改图片外显
-        else if (item.elementType === 2) {
-            pluginLog('尝试修改图片外显')
-            try {
-                const subtype = item.picElement.picSubType;//subType 1是表情包，0是图片
-                const useRandom = subtype === 1 ? config.isMemeOTuseRandom : config.isPicOTuseRandom;
-                const outsideText = subtype === 1 ? config.memeOutsideText : config.picOutsideText;
-
-                let summary = outsideText;
-                if (useRandom)
-                    summary = (await getRandomText()) || outsideText;
-                item.picElement.summary = summary;
-            } catch (e) {
-                console.error(`获取随机文本或设置外显文字时发生错误: ${e}`);
+    for (const p of payload) {
+        for (let item of p.msgElements) {
+            //说明消息内容是文字类
+            if (item.elementType === 1) {
+                //修改解密消息
+                //item.textElement.summary='嘻嘻哈哈'   //这个修改没有用
             }
 
+            //说明消息内容是图片类，则修改图片外显
+            else if (item.elementType === 2) {
+                pluginLog('尝试修改图片外显')
+                try {
+                    const subtype = item.picElement.picSubType;//subType 1是表情包，0是图片
+                    const useRandom = subtype === 1 ? config.isMemeOTuseRandom : config.isPicOTuseRandom;
+                    const outsideText = subtype === 1 ? config.memeOutsideText : config.picOutsideText;
 
-            // pluginLog('修改后的,msgElements为')
-            // for (let item of args[3][1][1].msgElements) {
-            //     console.log(item)
-            // }
-        } else if (item.elementType === 11) {//11是marketFaceElement，是商城表情，用表情包的外显或者随机外显吧
-            try {
-                pluginLog('尝试修改商城表情外显')
+                    let summary = outsideText;
+                    if (useRandom)
+                        summary = (await getRandomText()) || outsideText;
+                    item.picElement.summary = summary;
+                } catch (e) {
+                    console.error(`获取随机文本或设置外显文字时发生错误: ${e}`);
+                }
 
-                const useRandom = config.isMemeOTuseRandom
-                const outsideText = config.memeOutsideText
 
-                let summary = outsideText;
-                if (useRandom)
-                    summary = (await getRandomText()) || outsideText;
-                item.marketFaceElement.faceName = summary;
+                // pluginLog('修改后的,msgElements为')
+                // for (let item of args[3][1][1].msgElements) {
+                //     console.log(item)
+                // }
+            } else if (item.elementType === 11) {//11是marketFaceElement，是商城表情，用表情包的外显或者随机外显吧
+                try {
+                    pluginLog('尝试修改商城表情外显')
 
-            } catch (e) {
-                pluginLog(`获取随机文本或设置外显文字时发生错误: ${e}`);
+                    const useRandom = config.isMemeOTuseRandom
+                    const outsideText = config.memeOutsideText
+
+                    let summary = outsideText;
+                    if (useRandom)
+                        summary = (await getRandomText()) || outsideText;
+                    item.marketFaceElement.faceName = summary;
+
+                } catch (e) {
+                    pluginLog(`获取随机文本或设置外显文字时发生错误: ${e}`);
+                }
             }
         }
     }
@@ -98,11 +102,11 @@ async function ipcMsgModify(args) {
 const textElement = {
     elementType: 1,
     elementId: '',
-    textElement: {content: '测试', atType: 0, atUid: '', atTinyId: '', atNtUid: ''}
+    textElement: { content: '测试', atType: 0, atUid: '', atTinyId: '', atNtUid: '' }
 }
 
 
-module.exports = {ipcModifyer}
+module.exports = { ipcModifyer }
 //插入一个卡片消息
 // args[3][1][1].msgElements.push({
 //     elementType: 10,
